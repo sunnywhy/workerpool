ae28b8b3a9f091e5112a6021325eabb143e4ebb7
sunnywhy <sunnywhy@gmail.com>
2022-01-26 10:31:23 -0500

    create the pool

--- pool.go
+++ pool.go
@@ -1,14 +1,61 @@
 package pool
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
+
+const (
+	defaultCapacity = 10
+	maxCapacity     = 50
+)
 
 type Pool struct {
-	capacity int // the size of the worker pool
-	active chan struct{} // active channel
-	tasks chan Task // tasks channel
+	capacity int           // the size of the worker pool
+	active   chan struct{} // active channel
+	tasks    chan Task     // tasks channel
 
-	wg sync.WaitGroup // used for waiting all workers exit when destroy the pool
-	quit chan struct{} // used to notify all workers exit
+	wg   sync.WaitGroup // used for waiting all workers exit when destroy the pool
+	quit chan struct{}  // used to notify all workers exit
 }
 
 type Task func()
+
+func New(capacity int) *Pool {
+	if capacity <= 0 {
+		capacity = defaultCapacity
+	}
+	if capacity > maxCapacity {
+		capacity = maxCapacity
+	}
+
+	p := &Pool{
+		capacity: capacity,
+		tasks:    make(chan Task),
+		quit:     make(chan struct{}),
+		active:   make(chan struct{}),
+	}
+	fmt.Printf("workerpool start\n")
+	go p.run()
+	return p
+}
+
+func (p *Pool) run() {
+	idx := 0
+
+	for {
+		select {
+		case <-p.quit:
+			return
+		case p.active <- struct{}{}:
+			// create a new worker
+			idx++
+			p.newWorker(idx)
+		}
+
+	}
+}
+
+func (p *Pool) newWorker(id int) {
+	
+}
